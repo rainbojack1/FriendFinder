1d559be2552779f0ec6a073857e300b0f4deaf2b
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-08-26 21:22:35 -0400

    Filled in the beginning logic for express and set the html routes.

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,1 +1,16 @@
-htmlRoutes.js
+//Routes
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/home", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-server.js
+const express = require("express");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8008;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, function(){
+    console.log("Listening on http://localhost:" + PORT);
+});
+
